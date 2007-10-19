36982564b5fea4865c27898245bc70618d7a7a78
blake.garretson <blake.garretson@59246edd-9e1a-0410-98de-79dcf2039bee>
2007-10-19 18:42:23 +0000

    Removed file/folder

--- dev-old/image_src/png2mod.py
+++
@@ -1,9 +0,0 @@
-import glob, cPickle, base64, zlib
-
-files = glob.glob("*.png")
-outfile = file('../images.py','w')
-outfile.write('catalog = {} \n\n')
-for f in files:
-    outfile.write('catalog["%s"] = """\n' % f[:-4])
-    outfile.write(base64.encodestring(file(f, "rb").read()))
-    outfile.write('"""\n\n')
