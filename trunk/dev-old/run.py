0ce6722ba8e136cca6b82ab8f28aa43d7a4efa64
blake.garretson <blake.garretson@59246edd-9e1a-0410-98de-79dcf2039bee>
2007-10-19 18:42:23 +0000

    Removed file/folder

--- trunk/dev-old/run.py
+++
@@ -1,7 +0,0 @@
-import profile, chord#, chord2
-
-#profile.run("""print chord2.Voicings("C", reach=2)""")
-#print "============================================================="
-profile.run("""print chord.Voicings("C13", reach=2)""")
-#print chord.Voicings("C", reach=2)
-raw_input("")
